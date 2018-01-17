5a15828391f1fadcb73da8d5776e1c55d4882aa3
ChinJL <33471703+ChinJL@users.noreply.github.com>
2018-01-17 15:09:32 +0800

    Update

---
+++ GE Assignment (Turn Based)/Assets/Script/SceneControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void SceneChange () {
+		SceneManager.LoadScene (0);
+	}
+
+	public void PlayButton ()
+	{
+		SceneManager.LoadScene (1);
+	}
+}
